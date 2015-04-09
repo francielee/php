0a9748208bf5821cf821c230fcfdeb4f0894efea
Franciele de Medeiros Nascimento <franciele.medeiroos@gmail.com>
2015-04-09 09:55:17 -0300

    Include x Require

---
+++ includeRequire/contato.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Aula PHP</title>
+	</head>
+	<body>
+
+	<?php include("menu.php"); ?>
+		<div>
+			<h1>Contato</h1>
+		</div>
+	</body>
+
+</html>
---
+++ includeRequire/index.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>Aula PHP</title>
+	</head>
+	<body>
+
+		<?php require_once("Menu.php");?>
+		<!--Quando o require não encontra o arquivo ele mostra o erro e mata a aplicação, já o include mostra o erro e continua a aplicação
+		Quando adiciono "_once" ele não deixa que aquele arquivo repita. Ex.: Sem querer coloco pra mostrar o mesmo arquivo por 2 vezes, o once faz com que mostre apenas uma vez-->
+		
+
+
+		</div>
+		<h1>Home</h1>
+		<?php echo "Ola mundo";?>
+	</body>
+
+</html>
